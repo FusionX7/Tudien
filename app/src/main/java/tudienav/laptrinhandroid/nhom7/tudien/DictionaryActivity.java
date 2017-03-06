5943b299f130c808ac3bf8cb6a60e374e3138dad
FusionX7 <badang789@gmail.com>
2017-03-06 20:47:37 +0700

    fix

--- app/src/main/java/tudienav/laptrinhandroid/nhom7/tudien/DictionaryActivity.java
+++ app/src/main/java/tudienav/laptrinhandroid/nhom7/tudien/DictionaryActivity.java
@@ -12,7 +12,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -71,11 +70,6 @@ public class DictionaryActivity extends AppCompatActivity implements LoaderManag
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getm
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
